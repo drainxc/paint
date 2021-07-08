030a224df36ff14f7713872996e7be3de9877072
ldh7228 <ldh7228@gmail.com>
2021-07-08 09:40:27 +0900

    Create opacity

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const brushControl = document.getElementById('brushControl');
 const size = document.getElementById('size');
+const opacity = document.getElementById('opacity');
+const opacityNumber = document.getElementById('opacityNumber');
 const brush = document.getElementById('brush');
 const eraser = document.getElementById('eraser');
 const fill = document.getElementById('fill');
 const reset = document.getElementById('reset');
+const elementToChange = document.getElementsByTagName("body")[0];
+elementToChange.style.cursor = "url('./asset/paint-brush.png'), auto";
 
+opacityNumber.innerHTML = `${document.getElementById('opacity').value}`;
 size.innerHTML = `${document.getElementById('brushControl').value}`;
 canvas.width = 600;
 canvas.height = 700;
@@ -15,7 +20,6 @@ ctx.fillStyle = "white";
 ctx.fillRect(0, 0, 600, 700);
 let painting = false;
 ctx.lineWidth = 10;
-
 
 function start() {
     painting = true;
@@ -47,11 +51,13 @@ function range() {
 function eraserTool() {
     ctx.strokeStyle = "white";
     ctx.fillStyle = "white";
+    elementToChange.style.cursor = "url('./asset/eraser.png'), auto";
 }
 
 function brushTool() {
     ctx.strokeStyle = "black";
     ctx.fillStyle = "black";
+    elementToChange.style.cursor = "url('./asset/paint-brush.png'), auto";
 }
 
 function fillTool() {
@@ -64,11 +70,18 @@ function resetTool() {
     ctx.fillRect(0, 0, 600, 700);
 }
 
+function opacityControl() {
+    const opacityFigures = document.getElementById('opacity').value / 100;
+    ctx.globalAlpha = opacityFigures;
+    opacityNumber.innerHTML = `${opacityFigures * 100}`;
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", start);
 canvas.addEventListener("mouseup", stop);
 canvas.addEventListener("mouseleave", stop);
 brushControl.addEventListener("input", range);
+opacity.addEventListener('input', opacityControl);
 eraser.addEventListener('click', eraserTool);
 brush.addEventListener('click', brushTool);
 fill.addEventListener('click', fillTool);
